bea51f61d4e77ff638bf6e1b45050e0d38c96c41
derekhargest <derekhargest@gmail.com>
2016-01-18 18:51:11 -0500

    addition of title

--- wp-content/themes/gandco/header.php
+++ wp-content/themes/gandco/header.php
@@ -12,6 +12,7 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<title>Gunther &amp; Co. - ENTER TO WIN A CHEF’S DINNER!</title>
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
